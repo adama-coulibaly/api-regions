5ad7986a41f9ff2a46f8a04a5d8f2fef499ebece
adcoulibaly <coulibalyadamabekaye03@gmail.com>
2022-12-15 09:36:57 +0000

    Mon premier depot

---
+++ src/main/java/com/apiregions/apiregions/ApiRegionsApplication.java
@@ -0,0 +1,13 @@
+package com.apiregions.apiregions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRegionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRegionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apiregions/apiregions/ApiRegionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiregions.apiregions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRegionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
